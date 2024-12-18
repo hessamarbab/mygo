28b8f1112be8b0bdf81f9fdb434da10f7465e57e
Hessam Arbab <hessam.arbab@gmail.com>
2024-12-18 23:34:04 +0330

    fixed an err and used context bind instead json

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,25 +1,27 @@
 package controllers
 
 import (
-	"encoding/json"
 	"github.com/labstack/echo/v4"
 	"net/http"
 	"sika-hessam/database"
 	"sika-hessam/models"
 )
 
+type GetUserRequestBody struct {
+	Id string `json:"id"`
+}
+
 func GetUser(c echo.Context) error {
 	db := database.GetDB()
-	json_map := make(map[string]string)
-	err := json.NewDecoder(c.Request().Body).Decode(&json_map)
-	if err != nil {
+	var reqBody GetUserRequestBody
+	if err := c.Bind(&reqBody); err != nil {
 		return c.JSON(http.StatusBadRequest, struct {
 			Message string `json:"message"`
 		}{"Bad request"})
 	} else {
 		var user models.User
-		user.Id = json_map["id"]
-		err := db.Model(&models.User{}).Preload("CreditCards").Find(&user, "id = ?", json_map["id"]).Error
+		user.Id = reqBody.Id
+		err := db.Model(&models.User{}).Preload("Addresses").Find(&user, "id = ?", reqBody.Id).Error
 
 		if err != nil {
 			return c.JSON(http.StatusNotFound, struct {
