c6f824b85de5acff72724852e5e8dc1e4e122867
Hessam Arbab <hessam.arbab@gmail.com>
2025-01-06 15:22:43 +0330

    changed main ppage

--- controllers/homePage.go
+++ controllers/homePage.go
@@ -7,11 +7,5 @@ import (
 
 func GetHome(c echo.Context) error {
 	return c.HTML(http.StatusOK,
-		"<html><h1>Hi use /user to get user by id like this:</h1>"+
-			"<h3><pre>           (use method GET & this headers "+
-			"Accept: \"application/json\" ,  Content-Type:\"application/json\" )\n\n"+
-			"			{\n"+
-			"				\"id\": \"35b75a6b-0183-43e1-bdd0-3cd062dc6935\"\n"+
-			"			}</pre></h3>"+
-			"</html>")
+		"Server is running; you could use apis!")
 }
